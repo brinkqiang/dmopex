8a3a95b7f8e66d118d12cb1aa1fde1946f40431a
brinkqiang <brink.qiang@gmail.com>
2025-05-28 12:14:25 +0800

    ad

--- test/dmopextest/dmopextest.cpp
+++ test/dmopextest/dmopextest.cpp
@@ -25,44 +25,15 @@ protected:
 };
 
 // 示例1：使用成员变量
-class Point2D : public Opex::Operable<Point2D, &Point2D::x, &Point2D::y> {
+
+class Point2D {
 public:
     double x, y;
-    
+
     Point2D(double x = 0, double y = 0) : x(x), y(y) {}
 };
 
-// 示例2：使用getter函数
-class Vector3D : public Opex::Operable<Vector3D, &Vector3D::getX, &Vector3D::getY, &Vector3D::getZ> {
-private:
-    double m_x, m_y, m_z;
-    
-public:
-    Vector3D(double x = 0, double y = 0, double z = 0) : m_x(x), m_y(y), m_z(z) {}
-    
-    double& getX() { return m_x; }
-    double& getY() { return m_y; }
-    double& getZ() { return m_z; }
-    
-    const double& getX() const { return m_x; }
-    const double& getY() const { return m_y; }
-    const double& getZ() const { return m_z; }
-};
-
-// 示例3：混合使用（成员变量 + lambda）
-class Complex : public Opex::Operable<Complex, &Complex::real, &Complex::imag> {
-public:
-    double real, imag;
-    
-    Complex(double r = 0, double i = 0) : real(r), imag(i) {}
-};
-
-// 示例4：使用便利宏
-class Rectangle : public Opex::Operable<Rectangle, OPEX_MEMBER(width), OPEX_MEMBER(height)> {
-public:
-    double width, height;
-    Rectangle(double w = 0, double h = 0) : width(w), height(h) {}
-};
+PP_OPERATOR_DEF_V2(Point2D, x, y);
 
 TEST_F(frame_dmopex, init)
 {
@@ -71,11 +42,4 @@ TEST_F(frame_dmopex, init)
     
     Point2D p3 = p1 + p2;  // p3.x = 4.0, p3.y = 6.0
     p1 += p2;              // p1.x = 4.0, p1.y = 6.0
-    
-    bool equal = (p1 == p3);  // true
-    bool less = (p1 < p2);    // false
-    
-    Vector3D v1(1, 2, 3);
-    Vector3D v2(4, 5, 6);
-    Vector3D v3 = v1 * v2;  // 各分量相乘
 }
